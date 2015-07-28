c5ba0a0e9ae44337e0db73db6e26f9a0f0b8dabc
David Brear <david.brear@tabbedout.com>
2015-07-28 10:10:28 -0400

    updated index.js to see if I can get the module to export

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var SortHeader = require('./lib/SortHeader.js');
 var SortTable = require('./lib/SortTable.js');
-module.SortHeader = SortHeader;
-module.SortTable = SortTable;
+module.exports = SortTable;
 
